d96c919b2c5f2311363ab6399b938bd2dc91036d
masuhara <info@masuhara.net>
2015-10-20 00:11:18 +0900

    Initial Commit

---
+++ TabbarSampler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabbarSampler
+//
+//  Created by Masuhara on 2015/10/20.
+//  Copyright © 2015年 masuhara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
